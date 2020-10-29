b97e8beaa072abcf698331c2a7522bf39d4c807b
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-29 16:57:10 -0600

    csReverseIntegerBits complete

--- csReverseIntegerBits.py
+++ csReverseIntegerBits.py
@@ -8,8 +8,13 @@
 # csReverseIntegerBits(0) -> 0
 
 def csReverseIntegerBits(n):
-
-
-
-
+    #convert n to binary
+    #convert binary to string & reverse it
+    #convert reversed string to binary
+    #convert back to decimal
+    bit_n = ''.join([bin(n)])
+    bin_string = bit_n[2:]
+    reversed_char = int(bin_string[::-1], 2)
+    return reversed_char
+    
 print(csReverseIntegerBits(417))
